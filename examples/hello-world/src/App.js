0991b142f536e7f7373fb2790d5ee3db1504b743
Jean Martineau-Figuette <jean.martineaufiguette@cegedim-activ.com>
2017-11-13 22:16:52 +0100

    regression when typing a date

--- examples/hello-world/src/App.js
+++ examples/hello-world/src/App.js
@@ -4,28 +4,56 @@ import moment from 'moment';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
+const EN_DATE_FORMAT = 'MM/DD/YYYY';
+const FR_DATE_FORMAT = 'DD/MM/YYYY';
+
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      startDate: moment()
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            startDate: null,
+            endDate: null,
+            dateFormat: EN_DATE_FORMAT,
+        };
+    }
 
-  render() {
-    return (
-      <DatePicker
-        selected={this.state.startDate}
-        onChange={this.handleChange.bind(this)}
-      />
-    );
-  }
+    changeLanguage = () => {
+        const newDateFormat = this.state.dateFormat === EN_DATE_FORMAT ? FR_DATE_FORMAT : EN_DATE_FORMAT;
+        this.setState({ dateFormat: newDateFormat });
+    }
 
-  handleChange(date) {
-    this.setState({
-      startDate: date
-    });
-  }
+    render() {
+        return (
+            <div>
+                <DatePicker
+                    name='startDate'
+                    selected={this.state.startDate}
+                    onChange={this.handleChangeStart}
+                    dateFormat={this.state.dateFormat}
+                />
+                <DatePicker
+                    name='endDate'
+                    selected={this.state.endDate}
+                    onChange={this.handleChangeEnd}
+                    dateFormat={this.state.dateFormat}
+                />
+                <button onClick={this.changeLanguage} >Switch</button>
+                {this.state.dateFormat}
+            </div>
+        );
+    }
+
+    handleChangeStart = (date) => {
+        this.setState({
+            startDate: date
+        });
+    }
+
+    handleChangeEnd = (date) => {
+        this.setState({
+            endDate: date
+        });
+    }
 }
 
 export default App;
